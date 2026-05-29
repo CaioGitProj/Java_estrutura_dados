7b8eb20f3b2fd7f71c75c35d84688c554ed64ef2
Caio Dias <caiodevgit@gmail.com>
2026-05-29 17:13:03 -0300

    Usando o método desempilha

--- Pilhas/src/Main.java
+++ Pilhas/src/Main.java
@@ -12,6 +12,9 @@ public class Main
         }
 
         System.out.println(pilha);
+
+        pilha.desempilha();
+
         System.out.println(pilha.getTamanho());
         System.out.println(pilha.estaVazia());
         System.out.println(pilha.topo());
